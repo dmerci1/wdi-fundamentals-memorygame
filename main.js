75fa41a35762f726eaa3271790d3895f58d4f5dd
Daniel Mercia <danielmercia@Daniels-iMac.home>
2017-07-19 15:30:22 -0400

    created game logic

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-var cardOne = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped" + " " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped" + " " + cardTwo);
+//if (cardsInPlay.length === 2) {
+    //alert("yes");
+//}
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You've found a match!");
+}
+else {
+	alert("Sorry, try again");
+}
+
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
 console.log("User flipped " + cardOne)
 
-console.log("User flipped " + cardThree)
+console.log("User flipped " + cardThree)*/
